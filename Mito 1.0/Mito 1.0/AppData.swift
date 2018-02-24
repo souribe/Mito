4fa566676bca7fa056741392aa09ce8c91c00610
Benny Souriyadeth <bennys@uw.edu>
2018-02-24 15:43:06 -0800

    add ios

---
+++ Mito 1.0/Mito 1.0/AppData.swift
@@ -0,0 +1,13 @@
+//
+//  AppData.swift
+//  Mito 1.0
+//
+//  Created by Benny on 2/23/18.
+//  Copyright © 2018 Benny Souriyadeth. All rights reserved.
+//
+
+import UIKit
+
+class AppData: NSObject {
+    static let shared = AppData()
+}
